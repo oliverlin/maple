96ba6db5ceb2646609b7c16f8450e70dc291c83d
Oliver <linchong223@gmail.com>
2015-06-24 16:16:47 +0800

    Toggle buttons if interval is going

--- app/counter.js
+++ app/counter.js
@@ -6,7 +6,8 @@ var Counter = React.createClass({
     return(
       {
         count_number: 0,
-        interval: 0
+        interval: 0,
+        interval_is_going: false
       }
     );
   },
@@ -21,16 +22,18 @@ var Counter = React.createClass({
       this._increaseCount();
     }, 1000);
     this.setState({interval: interval});
+    this.setState({interval_is_going: true});
   },
   _stopCount: function(){
     clearInterval(this.state.interval);
+    this.setState({interval_is_going: false});
   },
   render(){
     return (
       <div>
         {this.state.count_number}
-        <button onClick={this._stopCount}>Stop</button>
-        <button onClick={this._startCount}>Go</button>
+        <button onClick={this._stopCount} disabled={!this.state.interval_is_going}>Stop</button>
+        <button onClick={this._startCount} disabled={this.state.interval_is_going}>Go</button>
       </div>
     );
   }
