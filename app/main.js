4054dcb64ef45e17a408f4a633bc9a38f3ef55c5
Oliver <linchong223@gmail.com>
2015-06-26 17:11:16 +0800

    Add basic route

--- app/main.js
+++ app/main.js
@@ -41,6 +41,35 @@ var Main = React.createClass({
     }
 });
 
+
+var App = React.createClass({
+  render(){
+    var Child;
+    switch (this.props.route) {
+      case '': Child = Main; break;
+      case 'item': Child = Item; break;
+      case 'round': Child = Round; break;
+      case 'counter': Child = Counter; break;
+      default: Child = NoMatch;
+    }
+
+    return(
+      <div>
+        <div>{this.props.route || 'APP'}</div>
+        <Child />
+      </div>
+    );
+  }
+});
+
+var NoMatch = React.createClass({
+  render(){
+    return(
+      <div>NoMatch</div>
+    );
+  }
+});
+
 window.Person = class Person {
   constructor(firstName, lastName){
     this.firstName = firstName;
@@ -56,4 +85,12 @@ window.Person = class Person {
   }
 };
 
-React.render(<Main />, document.body);
+
+function render () {
+  var route = window.location.hash.substr(1);
+  React.render(<App route={route} />, document.body);
+}
+
+window.addEventListener('hashchange', render);
+render(); // render initially
+
